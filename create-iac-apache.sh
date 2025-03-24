11471adf2e13fcb5e50d5fd885860999e2edab62
Ramon Risério (DevOps Engineer) <ramon.riserio@devops.engineer>
2025-03-24 21:57:17 +0000

    Improving a little bit

--- create-iac-apache.sh
+++ create-iac-apache.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# O próximo comando garante que os pipelines falhem se qualquer comando dentro
+# deles falhar e trata váriaveis não definidas como erros
+set -euxo pipefail
+
 echo "Atualizando o servidor..."
 apt-get update
 apt-get upgrade -y
@@ -10,6 +14,8 @@ echo "Baixando e copiando os arquivos da aplicação..."
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
-rm main.zip
 cp -R /tmp/linux-site-dio-main/* /var/www/html/
+
+echo "Apagando arquivos e diretórios temporários..."
+rm /tmp/main.zip
 rm -rf /tmp/linux-site-dio-main/
